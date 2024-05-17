62615d611f42e781175305051ef212219288d171
jesus-car <jesus.garcia2611@hotmail.com>
2024-05-16 20:48:56 -0500

    last change

--- src/App.java
+++ src/App.java
@@ -8,19 +8,19 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        DB.crearTabla();
-        OdontologoService odontologoService = new OdontologoService();
-        odontologoService.guardarOdontologo(new Odontologo("12345ABDC", "Jesus", "Garcia"));
-        List<Odontologo> odontologos = odontologoService.buscarTodos();
-
-        odontologos.forEach(o -> System.out.println(o.getNombre()));
-
-        Odontologo odontologo = new Odontologo(1, "12345", "Pedro", "Perez");
-        OdontologoMemoryService odontologoMemoryService = new OdontologoMemoryService();
-        odontologoMemoryService.Guardar(odontologo);
-        List<Odontologo> lista = odontologoMemoryService.Listar();
-
-        lista.forEach(o -> System.out.println(o.getNombre()));
+//        DB.crearTabla();
+//        OdontologoService odontologoService = new OdontologoService();
+//        odontologoService.guardarOdontologo(new Odontologo("12345ABDC", "Jesus", "Garcia"));
+//        List<Odontologo> odontologos = odontologoService.buscarTodos();
+//
+//        odontologos.forEach(o -> System.out.println(o.getNombre()));
+//
+//        Odontologo odontologo = new Odontologo(1, "12345", "Pedro", "Perez");
+//        OdontologoMemoryService odontologoMemoryService = new OdontologoMemoryService();
+//        odontologoMemoryService.Guardar(odontologo);
+//        List<Odontologo> lista = odontologoMemoryService.Listar();
+//
+//        lista.forEach(o -> System.out.println(o.getNombre()));
 
     }
 }
--- src/OdontologoServiceTest.java
+++ src/OdontologoServiceTest.java
@@ -1,4 +1,6 @@
+import dao.DB;
 import models.Odontologo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import services.OdontologoService;
 
@@ -7,10 +9,24 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OdontologoServiceTest {
+    @BeforeEach
+    public void setUp() {
+        DB.crearTabla();
+    }
+
     @Test
     public void buscarTodosTest() {
         OdontologoService odontologoService = new OdontologoService();
         List<Odontologo> odontologos = odontologoService.buscarTodos();
+        assertEquals(4, odontologos.size());
+    }
+
+    @Test
+    public void guardarOdontologoTest() {
+        OdontologoService odontologoService = new OdontologoService();
+        odontologoService.guardarOdontologo(new Odontologo("54321ABDC", "Josmar", "Diaz"));
+        List<Odontologo> odontologos = odontologoService.buscarTodos();
         assertEquals(5, odontologos.size());
+        odontologos.forEach(o -> System.out.println(o.getNombre()));
     }
 }
